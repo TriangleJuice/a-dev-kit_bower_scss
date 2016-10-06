84ae194ddbfb7c242bb07e444412ef991f8127c1
Jasper Van Proeyen <trianglejuice@mac.com>
2016-10-06 09:32:12 +0200

    * Fixed the footer that was floating all over the screen in IE * Random cleanup

--- src/app/main/main.controller.spec.js
+++ src/app/main/main.controller.spec.js
@@ -3,13 +3,10 @@
 
   describe('controllers', function(){
     var vm;
-    var $timeout;
-    var toastr;
 
     beforeEach(module('aDevKitBowerScss'));
     beforeEach(inject(function(_$controller_) {
       vm = _$controller_('MainController');
-      $timeout = _$timeout_;
     }));
 
     it('should have a timestamp creation date', function() {
